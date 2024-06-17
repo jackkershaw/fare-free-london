6afdc5e8882c76b4cda295a3ee9206773784cefa
jones58 <108466184+jones58@users.noreply.github.com>
2024-06-17 16:55:39 +0100

    edit footer content

--- components/footer.tsx
+++ components/footer.tsx
@@ -6,7 +6,10 @@ export default function Footer() {
       <div className="p-5 lg:p-10 lg:mx-10 font-serif justify-between space-y-10 lg:space-y-0 flex flex-col lg:flex-row lg:items-end">
         <SocialIcons />
         <div className="flex items-center">
-          <p className="text-xl font-sans ">Fare Free London</p>
+          <p className="text-xl font-sans ">
+            Fare Free London - Campaign for Free Public Transport in
+            London
+          </p>
         </div>
         <div className="lg:text-right">
           <p className="py-2 lg:py-0">
