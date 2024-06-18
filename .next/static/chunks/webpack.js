d72fd5ba8abab2ce6ed5c9176236c3148bfa48b5
jones58 <108466184+jones58@users.noreply.github.com>
2024-06-18 18:00:38 +0100

    edit appearance of articles and nextjs build

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5f7e4058a583f6f1"; }
+/******/ 		__webpack_require__.h = function() { return "7cc18c9aca76aea4"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/more-stories-preview.tsx
+++ components/more-stories-preview.tsx
@@ -13,7 +13,7 @@ export default function PostPreview({
     : "";
 
   return (
-    <div className="mb-6 lg:mb-0 bg-darkerBg bg-opacity-30 hover:bg-opacity-50 p-10">
+    <div className="mb-6 lg:mb-0 bg-darkerBg bg-opacity-30 hover:bg-opacity-50 p-8 rounded-lg">
       <div className="flex flex-col space-y-2 lg:space-y-3">
         {coverImage && (
           <CoverImage
