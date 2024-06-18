eb32b9b32da165a24df0fffeb6f862d1fdb1b95f
jones58 <108466184+jones58@users.noreply.github.com>
2024-06-18 16:28:25 +0100

    npm build

---
+++ .next/server/chunks/593.js
@@ -0,0 +1,157 @@
+exports.id=593,exports.ids=[593],exports.modules={8149:e=>{e.exports={content:"about_content__ADrHp","prose-lg":"about_prose-lg__vj_j5"}},8564:(e,t,a)=>{"use strict";a.d(t,{Z:()=>r});var n=a(997);function s(){return(0,n.jsxs)("div",{className:"flex flex-row lg:items-end justify-between space-x-5 lg:space-x-10 pb-4 lg:pb-0 text-3xl",children:[n.jsx("a",{href:"https://www.twitter.com/healthworkersu1",target:"_blank",rel:"noopener noreferrer","aria-label":"link to the Fare Free London Twitter page",className:"flex items-end justify-end",children:n.jsx("span",{className:"icon-[mdi--twitter] text-link hover:text-hover active:text-link p-5 lg:p-0"})}),n.jsx("a",{href:"https://www.instagram.com/vitalsignsmag/",target:"_blank",rel:"noopener noreferrer","aria-label":"link to the Fare Free London Instagram page",className:"flex items-end justify-end",children:n.jsx("span",{className:"icon-[mdi--instagram] text-link hover:text-hover active:text-link p-5 lg:p-0"})}),n.jsx("a",{href:"https://vitalsignscontenteditor.co.uk/rss",target:"_blank",className:"flex items-end justify-end","aria-label":"link to the Fare Free London Magazine RSS feed",children:n.jsx("span",{className:"icon-[mdi--rss] text-link hover:text-hover active:text-link p-5 lg:p-0"})}),n.jsx("a",{"aria-label":"link to the Fare Free London email",href:"mailto:vitalsignsmag@proton.me",target:"_blank",className:"flex items-end justify-end",children:n.jsx("span",{className:"icon-[mdi--email] text-link hover:text-hover active:text-link p-5 lg:p-0",children:" "})})]})}function r(){return n.jsx("footer",{className:"bg-darkerBg w-full",children:(0,n.jsxs)("div",{className:"p-5 lg:p-10 lg:mx-10 font-serif justify-between space-y-10 lg:space-y-0 flex flex-col lg:flex-row lg:items-end",children:[n.jsx(s,{}),n.jsx("div",{className:"flex items-center",children:n.jsx("p",{className:"text-xl font-bold",children:"Fare Free London - Campaign for Free Public Transport in London"})}),(0,n.jsxs)("div",{className:"lg:text-right",children:[(0,n.jsxs)("p",{className:"py-2 lg:py-0",children:["Site by"," ",n.jsx("a",{href:"https://www.jackkershaw.net",target:"_blank",className:"font-bold text-link hover:text-hover active:text-link","aria-label":"link to Jack Kershaw's website",children:"Jack Kershaw."})]}),(0,n.jsxs)("p",{className:"py-2 lg:py-0",children:["Source on"," ",n.jsx("a",{href:"https://github.com/jones58/fare-free-london",target:"_blank",className:"font-bold text-link hover:text-hover active:text-link","aria-label":"link to the Fare Free London GitHub repository",children:"Github."})]})]})]})})}},5247:(e,t,a)=>{"use strict";a.a(e,async(e,n)=>{try{a.d(t,{Z:()=>g});var s=a(997),r=a(6689),i=a(1664),l=a.n(i),o=a(5675),c=a.n(o),d=a(6197),x=e([d]);function g(){let[e,t]=(0,r.useState)(!1);return s.jsx("header",{className:"bg-darkerBg lg:px-10 pb-5 mb-5 lg:mb-0",children:(0,s.jsxs)("div",{className:"lg:grid lg:grid-cols-2 lg:mx-10",children:[(0,s.jsxs)("div",{className:"flex flex-row items-end justify-between px-5 lg:px-0 lg:justify-start lg:space-y-10 bg-darkerBg",children:[s.jsx("button",{onClick:()=>t(!e),className:"lg:hidden text-5xl text-link pr-5 pb-8",type:"button","aria-label":"menu",children:e?s.jsx("span",{className:"icon-[mdi--arrow-up] text-link"}):s.jsx("span",{className:"icon-[mdi--hamburger-menu] text-link"})}),s.jsx(l(),{href:"/",className:"max-h-[220px] ",children:s.jsx("div",{className:"min-w-[250px] min-h-[250px] lg:min-w-[20vw] lg:min-h-[20vh] relative",children:s.jsx(c(),{src:"/images/banner-draft.png",alt:"logo",fill:!0,className:"object-contain object-center",sizes:"(100vw, 100vh)"})})})]}),s.jsx("div",{className:"flex flex-row lg:flex-col lg:justify-end lg:hidden",children:e&&s.jsx(d.motion.div,{className:"absolute lg:static space-y-8 justify-start w-full flex flex-col z-50 bg-darkerBg h-[80vh] pl-5 lg:pl-0",initial:{opacity:0,y:-20},animate:{opacity:1,y:0},exit:{opacity:0,y:-20},children:(0,s.jsxs)("div",{className:"flex flex-col space-y-12 text-4xl font-sans py-10 pl-5",children:[s.jsx(l(),{href:"/aims",className:"text-link hover:text-hover active:text-link","aria-label":"link to the aims page",children:"Our Aims"}),s.jsx(l(),{href:"/news?category=",className:"text-link hover:text-hover active:text-link","aria-label":"link to the news page",children:"Latest News"}),s.jsx(l(),{href:"/getinvolved",className:"text-link hover:text-hover active:text-link","aria-label":"link to the get involved page",children:"Get Involved"})]})})}),s.jsx("div",{className:"hidden lg:flex flex-row lg:flex-col lg:justify-end",children:s.jsx("div",{className:"space-x-10 space-y-8 lg:space-y-0 lg:items-center justify-end w-full flex flex-col lg:flex-row z-50 bg-darkerBg",children:(0,s.jsxs)("div",{className:"flex flex-row items-center space-x-10 text-3xl font-sans pb-6",children:[s.jsx(l(),{href:"/aims",className:"text-link hover:text-hover active:text-link","aria-label":"link to the aims page",children:"Our Aims"}),s.jsx(l(),{href:"/news",className:"text-link hover:text-hover active:text-link","aria-label":"link to the news page",children:"Latest News"}),s.jsx(l(),{href:"/getinvolved",className:"text-link hover:text-hover active:text-link","aria-label":"link to the get involved page",children:"Get Involved"})]})})})]})})}d=(x.then?(await x)():x)[0],n()}catch(e){n(e)}})},6836:(e,t,a)=>{"use strict";a.a(e,async(e,n)=>{try{a.d(t,{Z:()=>p});var s=a(997),r=a(5247),i=a(8564),l=a(8936),o=a(968),c=a.n(o),d=a(6197),x=a(9752),g=a(7451),m=e([r,d,x,g]);function p({children:e}){return(0,s.jsxs)("div",{children:[(0,s.jsxs)(c(),{children:[s.jsx("title",{children:"Fare Free London"}),s.jsx("meta",{name:"description",content:"Campaign for free public transport in London"})]}),s.jsx(x.Analytics,{}),s.jsx(g.SpeedInsights,{}),s.jsx(l.Z,{}),s.jsx(r.Z,{}),s.jsx(d.motion.div,{initial:"pageInitial",animate:"pageAnimate",variants:{pageInitial:{y:20,opacity:0,scale:.99},pageAnimate:{y:0,opacity:1,scale:1,transition:{duration:.4,ease:"easeInOut",when:"beforeChildren"}}},children:s.jsx("main",{className:"bg-lighterBg text-customRed-950 px-5 lg:px-10 lg:mx-10 min-h-[60vh] lg:mt-10 lg:py-5",children:e})}),s.jsx(i.Z,{})]})}[r,d,x,g]=m.then?(await m)():m,n()}catch(e){n(e)}})},8936:(e,t,a)=>{"use strict";a.d(t,{Z:()=>i});var n=a(997),s=a(968),r=a.n(s);function i(){return(0,n.jsxs)(r(),{children:[n.jsx("link",{rel:"apple-touch-icon",sizes:"180x180",href:"/favicon/apple-touch-icon.png"}),n.jsx("link",{rel:"icon",type:"image/png",sizes:"32x32",href:"/favicon/favicon-32x32.png"}),n.jsx("link",{rel:"icon",type:"image/png",sizes:"16x16",href:"/favicon/favicon-16x16.png"}),n.jsx("link",{rel:"manifest",href:"/favicon/site.webmanifest"}),n.jsx("link",{rel:"shortcut icon",href:"/favicon/favicon.ico"}),n.jsx("meta",{name:"theme-color",content:"#000"}),n.jsx("meta",{name:"msapplication-TileColor",content:"#000000"}),n.jsx("meta",{name:"description",content:"Campaign for free public transport in London"}),n.jsx("title",{children:"Fare Free London"}),n.jsx("meta",{name:"twitter:card",content:"summary_large_image"}),n.jsx("meta",{name:"twitter:title",content:"Fare Free london"}),n.jsx("meta",{name:"twitter:description",content:"Campaign for free public transport in London"}),n.jsx("meta",{name:"twitter:image",content:"/images/banner-draft.png"}),n.jsx("meta",{property:"og:type",content:"website"}),n.jsx("meta",{property:"og:url",content:"https://farefreelondon.org/"}),n.jsx("meta",{property:"og:title",content:"Fare Free London"}),n.jsx("meta",{property:"og:description",content:"Campaign for free public transport in London"}),n.jsx("meta",{property:"og:image",content:"/images/banner-draft.png"}),n.jsx("meta",{name:"viewport",content:"width=device-width, initial-scale=1"})]})}},8484:(e,t,a)=>{"use strict";a.d(t,{CP:()=>d,DT:()=>i,Y8:()=>x,_S:()=>c,ds:()=>l,h9:()=>r,vx:()=>o});let n=process.env.WORDPRESS_API_URL;async function s(e="",{variables:t}={}){let a=await fetch(n,{headers:{"Content-Type":"application/json"},method:"POST",body:JSON.stringify({query:e,variables:t})}),s=await a.json();if(s.errors)throw console.error(s.errors),Error("Failed to fetch API");return s.data}async function r(){let e=await s(`
+    {
+      posts(first: 10000) {
+        edges {
+          node {
+            slug
+          }
+        }
+      }
+    }
+  `);return e?.posts}async function i(e){let t=await s(`
+    query AllPosts {
+      posts(first: 10000, where: { orderby: { field: DATE, order: DESC } }) {
+        edges {
+          node {
+            title
+            categories {
+              nodes {
+                name
+              }
+            }
+            excerpt
+            slug
+            date
+            featuredImage {
+              node {
+                sourceUrl
+              }
+            }
+          }
+        }
+      }
+    }
+  `,{variables:{onlyEnabled:!e,preview:e}});return t?.posts}async function l(e){let t=await s(`
+    fragment AuthorFields on User {
+      name
+      firstName
+      lastName
+      avatar {
+        url
+      }
+    }
+    fragment PostFields on Post {
+      title
+      excerpt
+      slug
+      date
+      featuredImage {
+        node {
+          sourceUrl
+          altText
+        }
+      }
+      author {
+        node {
+          ...AuthorFields
+        }
+      }
+      pdf {
+        pdf {
+          node {
+          sourceUrl
+          mediaItemUrl
+        }
+        }
+      }
+      categories {
+        edges {
+          node {
+            name
+          }
+        }
+      }
+      tags {
+        edges {
+          node {
+            name
+          }
+        }
+      }
+    }
+    query PostBySlug($id: ID!, $idType: PostIdType!) {
+      post(id: $id, idType: $idType) {
+        ...PostFields
+        content
+      }
+      posts(first: 10000) {
+        edges {
+          node {
+            ...PostFields
+            content
+          }
+        }
+      }
+    }
+  `,{variables:{id:e,idType:"SLUG"}}),a=t.post.categories.edges.map(({node:e})=>e.name);t.posts.edges=t.posts.edges.filter(({node:t})=>t.slug!==e),t.posts.edges=t.posts.edges.filter(({node:e})=>e.categories.edges.map(({node:e})=>e.name).some(e=>a.includes(e)));let n=t.posts.edges.filter(({node:e})=>e.categories.edges.some(({node:e})=>a.includes(e.name)));return t.posts.edges=n.slice(0,3),t}async function o(e="about"){let t=await s(`
+  query getAboutPageContent {
+    pages(where: {name: "about"}) {
+      nodes {
+        content
+        featuredImage {
+          node {
+            altText
+            sourceUrl
+          }
+        }
+      }
+    }
+  }
+      `);return{content:t?.pages?.nodes[0]?.content||"",featuredImage:t?.pages?.nodes[0]?.featuredImage||""}}async function c(e="events"){let t=await s(`
+  query getAboutPageContent {
+    pages(where: {name: "events"}) {
+      nodes {
+        content
+        featuredImage {
+          node {
+            altText
+            sourceUrl
+          }
+        }
+      }
+    }
+  }
+      `);return{content:t?.pages?.nodes[0]?.content||"",featuredImage:t?.pages?.nodes[0]?.featuredImage||""}}async function d(){let e=await s(`
+query allcategories {
+  categories(first: 10000) {
+    edges {
+      node {
+        name
+      }
+    }
+  }
+}
+`);return e?.categories?.edges||[]}async function x(){let e=await s(`
+    query AllPosts {
+      posts(first: 10000, where: { orderby: { field: DATE, order: DESC }, categoryName: "Issues" }) {
+        edges {
+          node {
+            title
+            categories {
+              nodes {
+                name
+              }
+            }
+            excerpt
+            slug
+            date
+            featuredImage {
+              node {
+                sourceUrl
+              }
+            }
+          }
+        }
+      }
+    }
+  `);return e?.posts}},6814:(e,t,a)=>{"use strict";a.r(t),a.d(t,{default:()=>o});var n=a(997),s=a(4440),r=a.n(s),i=a(2865),l=a.n(i);a(9517);let o=function({Component:e,pageProps:t,router:a}){return n.jsx("main",{className:`${r().variable} ${l().variable}`,children:n.jsx(e,{...t})})}},7645:(e,t,a)=>{"use strict";a.r(t),a.d(t,{default:()=>r});var n=a(997),s=a(6859);function r(){return(0,n.jsxs)(s.Html,{lang:"en",children:[n.jsx(s.Head,{}),(0,n.jsxs)("body",{children:[n.jsx(s.Main,{}),n.jsx(s.NextScript,{})]})]})}},9517:()=>{}};
