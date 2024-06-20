71af241977dfd0eab6b88bbeccfd993f651a179a
jones58 <108466184+jones58@users.noreply.github.com>
2024-06-20 13:15:56 +0100

    edit frontpage content

--- components/frontpage.tsx
+++ components/frontpage.tsx
@@ -1,31 +1,48 @@
 export default function Frontpage() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen">
-      <h1 className="text-3xl font-bold">Fare Free London</h1>
-      Campaign for free public transport in London. Our organisation
-      brings together community groups, trade unions, environmentalists
-      and others who see free public transport as central to our vision
-      of london in which we want to live. What we stand for Opens the
-      city to all It is provided as a public service, just like health,
-      education and public parks, and is supported by public
-      investment. It is central to a vision of London as a city where
-      people, their health and the lives they live, come first.
-      Socially just Free public transport is socially just, supporting
-      the lowest-income families that are least likely to have a car.
-      It is better transport, underpinned by substantial investment,
-      with a secure, properly-rewarded workforce. It is one of the
-      drastic, demonstrative actions needed to tackle climate change
-      globally and air pollution locally. Model exists in 100+ cities
-      globally Public transport is already free in many smaller cities,
-      including Luxemburg, Tallinn (Estonia), Montpellier and Dunkerque
-      (France) and Albuquerque and Kansas City (USA). London can be the
-      first big global city to follow their example. We call on the
-      Mayor and the Greater London Authority (GLA) to provide free
-      public transport in London. The first step is to research ways to
-      implement it. We call on national government to support free
-      public transport in London, and around the country. The local
-      government finance rules need to be changed, so that local
-      authorities can raise money for it.
+    <div className="flex flex-col items-left justify-center min-h-screen">
+      <h1 className="text-3xl font-bold text-link">
+        Fare Free London
+      </h1>
+      <p>
+        Campaign for free public transport in London. Our organisation
+        brings together community groups, trade unions,
+        environmentalists and others who see free public transport as
+        central to our vision of london in which we want to live.
+      </p>
+      <h2>What We Stand For</h2>
+      <h3>Opens the City To All</h3>
+      <p>
+        It is provided as a public service, just like health, education
+        and public parks, and is supported by public investment. It is
+        central to a vision of London as a city where people, their
+        health and the lives they live, come first.
+      </p>
+      <h3>Socially Just</h3>
+      <p>
+        Free public transport is socially just, supporting the
+        lowest-income families that are least likely to have a car. It
+        is better transport, underpinned by substantial investment,
+        with a secure, properly-rewarded workforce. It is one of the
+        drastic, demonstrative actions needed to tackle climate change
+        globally and air pollution locally.
+      </p>
+      <h3>Model exists in 100+ cities globally</h3>
+      <p>
+        Public transport is already free in many smaller cities,
+        including Luxemburg, Tallinn (Estonia), Montpellier and
+        Dunkerque (France) and Albuquerque and Kansas City (USA).
+        London can be the first big global city to follow their
+        example.
+      </p>
+      <p>
+        We call on the Mayor and the Greater London Authority (GLA) to
+        provide free public transport in London. The first step is to
+        research ways to implement it. We call on national government
+        to support free public transport in London, and around the
+        country. The local government finance rules need to be changed,
+        so that local authorities can raise money for it.
+      </p>
     </div>
   );
 }
