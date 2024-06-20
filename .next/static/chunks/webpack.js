4528547b3a9b6937cabb3ab131147c1c30417901
jones58 <108466184+jones58@users.noreply.github.com>
2024-06-20 13:04:43 +0100

    edit social icon links

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "4f182a444f72857f"; }
+/******/ 		__webpack_require__.h = function() { return "4da708a49193fc61"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/social-icons.tsx
+++ components/social-icons.tsx
@@ -2,7 +2,7 @@ export default function SocialIcons() {
   return (
     <div className="flex flex-row lg:items-end justify-between space-x-5 lg:space-x-10 pb-4 pt-8 lg:pt-0 lg:pb-0 text-3xl">
       <a
-        href="https://www.twitter.com/healthworkersu1"
+        href="https://x.com/FareFreeLondon"
         target="_blank"
         rel="noopener noreferrer"
         aria-label="link to the Fare Free London Twitter page"
@@ -11,7 +11,16 @@ export default function SocialIcons() {
         <span className="icon-[mdi--twitter] text-link hover:text-hover active:text-link p-5 lg:p-0"></span>
       </a>
       <a
-        href="https://www.instagram.com/vitalsignsmag/"
+        href="https://www.facebook.com/profile.php?id=61556766211971"
+        target="_blank"
+        rel="noopener noreferrer"
+        aria-label="link to the Fare Free London Facebook page"
+        className="flex items-end justify-end"
+      >
+        <span className="icon-[mdi--facebook] text-link hover:text-hover active:text-link p-5 lg:p-0"></span>
+      </a>
+      <a
+        href="https://www.instagram.com/farefreelondon/"
         target="_blank"
         rel="noopener noreferrer"
         aria-label="link to the Fare Free London Instagram page"
@@ -29,7 +38,7 @@ export default function SocialIcons() {
       </a>
       <a
         aria-label="link to the Fare Free London email"
-        href="mailto:vitalsignsmag@proton.me"
+        href="mailto:info@farefreelondon.org"
         target="_blank"
         className="flex items-end justify-end"
       >
