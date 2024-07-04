db76b4c2d77f9efd1bba62f98b672dd32df2fe8c
jones58 <108466184+jones58@users.noreply.github.com>
2024-07-04 16:15:13 +0100

    remove .next

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{\"/_app\":[\"static/media/c653ff3f4a94aed3-s.p.otf\",\"static/media/2d070ef8c0e9908a-s.p.otf\"]},\"app\":{},\"appUsingSizeAdjust\":false,\"pagesUsingSizeAdjust\":true}"
