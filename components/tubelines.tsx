2d27668e555db1fb545471b3a4de10035c8aa81d
jones58 <108466184+jones58@users.noreply.github.com>
2024-07-06 13:15:23 +0100

    edit tubelines to original aspect ratio

--- components/tubelines.tsx
+++ components/tubelines.tsx
@@ -4,8 +4,8 @@ export default function TubeLines() {
   return (
     <div className="w-full bg-darkerBg">
       <Image
-        width="1080"
-        height="980"
+        width="3840"
+        height="791"
         src="/images/desktop.png"
         alt="intersecting coloured lines, reminiscent of tube lines"
         className="w-full h-40 pb-2"
