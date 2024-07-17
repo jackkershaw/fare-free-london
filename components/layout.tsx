d09da3bffdfa1768ed2ac7dd561ef88530c237da
jones58 <108466184+jones58@users.noreply.github.com>
2024-07-17 15:21:27 +0100

    edit animations so they lazily load

--- components/layout.tsx
+++ components/layout.tsx
@@ -2,7 +2,7 @@ import Header from "./header";
 import Footer from "./footer";
 import Meta from "./meta";
 import Head from "next/head";
-import { motion } from "framer-motion";
+import { motion, LazyMotion, domAnimation } from "framer-motion";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import TubeLines from "./tubelines";
@@ -15,32 +15,34 @@ export default function Layout({ children }) {
       <SpeedInsights />
       <Meta />
       <Header />
-      <motion.div
-        initial="pageInitial"
-        animate="pageAnimate"
-        variants={{
-          pageInitial: {
-            y: 20,
-            opacity: 0,
-            scale: 0.99,
-          },
-          pageAnimate: {
-            y: 0,
-            opacity: 1,
-            scale: 1,
-            transition: {
-              duration: 0.4,
-              ease: "easeInOut",
-              when: "beforeChildren",
+      <LazyMotion features={domAnimation}>
+        <motion.div
+          initial="pageInitial"
+          animate="pageAnimate"
+          variants={{
+            pageInitial: {
+              y: 20,
+              opacity: 0,
+              scale: 0.99,
             },
-          },
-        }}
-      >
-        <div className="bg-lighterBg text-black px-5 lg:px-10 lg:mx-10 min-h-[60vh] lg:mt-10 lg:py-5">
-          {children}
-        </div>
-        <BackToTop />
-      </motion.div>
+            pageAnimate: {
+              y: 0,
+              opacity: 1,
+              scale: 1,
+              transition: {
+                duration: 0.4,
+                ease: "easeInOut",
+                when: "beforeChildren",
+              },
+            },
+          }}
+        >
+          <div className="bg-lighterBg text-black px-5 lg:px-10 lg:mx-10 min-h-[60vh] lg:mt-10 lg:py-5">
+            {children}
+          </div>
+          <BackToTop />
+        </motion.div>
+      </LazyMotion>
       <Footer />
       <TubeLines />
     </div>
