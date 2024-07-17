38624037e770fcc429ff69679db214a6f9cae067
jones58 <108466184+jones58@users.noreply.github.com>
2024-07-17 15:02:04 +0100

    edit aims image size and position on desktop

--- pages/aims.tsx
+++ pages/aims.tsx
@@ -28,7 +28,7 @@ export default function About({ content, featuredImage }: Props) {
         </Head>
         <div>
           <h1 className="text-4xl">Aims</h1>
-          <div className="w-[30vw] h-auto mx-auto">
+          <div className="pt-4 lg:w-[40vw] lg:h-auto lg:mx-auto">
             <Image
               width="1080"
               height="980"
