0d10505eae0f6cf162e26d15d25926d0cd236dfc
Jack <jackkershaw@protonmail.com>
2024-08-04 11:43:15 +0100

    add self hosted umami analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,6 +9,7 @@ export default function Document() {
         src="https://cloud.umami.is/script.js"
         data-website-id="930adc75-e0eb-4c7a-85aa-4275ff4f519c"
       ></script>
+<script defer src="https://umami.jackkershaw.pp.ua/script.js" data-website-id="8b381dbb-2907-4867-b9d0-83ca338f5635"></script>
       <link
         rel="preconnect"
         href="https://va.vercel-scripts.com/"
