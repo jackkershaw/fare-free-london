b8e508a4abef309aa539717796f2f943cebc24b5
jackkershaw <hello@jackkershaw.net>
2025-06-05 22:29:38 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,58 +1,8 @@
-if (!URL.canParse(process.env.WORDPRESS_API_URL)) {
-  throw new Error(`
-    Please provide a valid WordPress instance URL.
-    Add to your environment variables WORDPRESS_API_URL.
-  `);
+function searchStudents(students: string[], query: string): string[] {
+  return  new Intl.DateTimeFormat("en-GB", {
+            minute: "numeric",
+            second: "numeric",
+          }).format(25 * 60 * 1000)
 }
-const { protocol, hostname, port, pathname } = new URL(
-  process.env.WORDPRESS_API_URL
-);
 
-/** @type {import('next').NextConfig} */
-module.exports = {
-  images: {
-    unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "admin.farefreelondon.org",
-        port: "",
-        pathname: "/**",
-      },
-    ],
-  },
-  // Cache static assets
-  async headers() {
-    return [
-      {
-        source: "/favicon/:path*",
-        // Cache all files in the /favicon directory
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "public, max-age=31536000", // Cache for 1 year
-          },
-        ],
-      },
-      {
-        source: "/fonts/:path*",
-        // Cache all files in the /fonts directory
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "public, max-age=31536000", // Cache for 1 year
-          },
-        ],
-      },
-      {
-        source: "/:path*",
-        headers: [
-          {
-            key: "X-Content-Type-Options",
-            value: "nosniff",
-          },
-        ],
-      },
-    ];
-  },
-};
+searchStudents(["Jack", "Jill", "Harry"], "J");
