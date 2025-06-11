cb54d3db1f176137e632fb55e6133b3311438b78
jackkershaw <hello@jackkershaw.net>
2025-06-11 22:42:45 +0100

    fix next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,4 @@
 if (!URL.canParse(process.env.WORDPRESS_API_URL)) {
-More actions
-
   throw new Error(`
 
     Please provide a valid WordPress instance URL.
@@ -8,30 +6,20 @@ More actions
     Add to your environment variables WORDPRESS_API_URL.
 
   `);
-
 }
 
 const { protocol, hostname, port, pathname } = new URL(
-
   process.env.WORDPRESS_API_URL
-
 );
-
-
 
 /** @type {import('next').NextConfig} */
 
 module.exports = {
-
   images: {
-
-
     unoptimized: true,
 
     remotePatterns: [
-
       {
-
         protocol: "https",
 
         hostname: "admin.farefreelondon.org",
@@ -39,79 +27,53 @@ module.exports = {
         port: "",
 
         pathname: "/**",
-
       },
-
     ],
-
   },
 
   // Cache static assets
 
   async headers() {
-
     return [
-
       {
-
         source: "/favicon/:path*",
 
         // Cache all files in the /favicon directory
 
         headers: [
-
           {
-
             key: "Cache-Control",
 
             value: "public, max-age=31536000", // Cache for 1 year
-
           },
-
         ],
-
       },
 
       {
-
         source: "/fonts/:path*",
 
         // Cache all files in the /fonts directory
 
         headers: [
-
           {
-
             key: "Cache-Control",
 
             value: "public, max-age=31536000", // Cache for 1 year
-
           },
-
         ],
-
       },
 
       {
-
         source: "/:path*",
 
         headers: [
-
           {
-
             key: "X-Content-Type-Options",
 
             value: "nosniff",
-
           },
-
         ],
-
       },
-
     ];
-
   },
-
 };
