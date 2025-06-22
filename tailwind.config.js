b6e144db732108d016a4d20606b7cfac8bb3e41b
jackkershaw <hello@jackkershaw.net>
2025-06-22 16:53:39 +0100

    add tailwind to app router

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ const { addDynamicIconSelectors } = require("@iconify/tailwind");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
