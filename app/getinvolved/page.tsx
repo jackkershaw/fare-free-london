16c93b84f48371f71e870cc4dddbc96a19b19029
jackkershaw <hello@jackkershaw.net>
2025-06-22 14:43:22 +0100

    move get involved page to app router

---
+++ app/getinvolved/page.tsx
@@ -0,0 +1,35 @@
+import Image from "next/image";
+import { getEventsPageContent } from "../../lib/api";
+import styles from "./GetInvolved.module.css";
+import Form from "../../components/form";
+
+export default async function Events() {
+  const data = await getEventsPageContent();
+  const content = data.content;
+  const featuredImage = data.featuredImage;
+
+  return (
+    <div>
+      <div>
+        <h1 className="text-4xl lg:text-left">Get Involved</h1>
+        <div className="pt-4 lg:w-[40vw] lg:h-auto lg:mx-auto">
+          <Image
+            width={1080}
+            height={980}
+            src={featuredImage.node.sourceUrl}
+            alt={featuredImage.node.altText}
+            className="rounded-lg object-cover lg:mx-auto"
+            priority
+          />
+        </div>
+        <div className={`${styles.content} `}>
+          <div dangerouslySetInnerHTML={{ __html: content }}></div>
+        </div>
+        <div className="mt-12 max-w-2xl mx-auto">
+          <h2 className="text-3xl mb-6">Contact Us</h2>
+          <Form />
+        </div>
+      </div>
+    </div>
+  );
+}
